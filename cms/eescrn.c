efa7412bf24452f216614d2df972fea0e0b0ca90
SanLorenzo59 <John.Doe@mail.net>
2022-10-21 12:37:04 +0200

    UI text constant "====>"

--- cms/eescrn.c
+++ cms/eescrn.c
@@ -115,7 +115,7 @@ static char colorsFor3270[16] = {
 };
 
 /* some UI text constants */
-static char *cmdArrow = "===>";
+static char *cmdArrow = "====>";
 static char *topOfFileText = "* * * Top of file * * *";
 static char *bottomOfFileText = "* * * End of file * * *";
 static char *prefixLocked = ".....";
