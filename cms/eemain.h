f11292c0b57490b4fe6bcc2e7b5309af672c8db1
SanLorenzo59 <John.Doe@mail.net>
2022-11-18 17:24:55 +0100

    #define VERSION "V1.3.0_221118-1307"

--- cms/eemain.h
+++ cms/eemain.h
@@ -21,7 +21,7 @@
 #define _EEMAINimported
 
 /* the version information for the tools, displayed in header/footer lines */
-#define VERSION "V1.3.0_221118-1053" /* branch AGPLv3_WIP */
+#define VERSION "V1.3.0_221118-1307" /* branch AGPLv3_WIP */
 
 /**************************************************************************************************/
 /* Two functions to help C programs avoid having any non-const static or global                   */
